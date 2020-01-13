85d340c93e42af0ebc41724e8816d9ed4aa1bf2f
berder <vincent.berder@al-enterprise.com>
2020-01-13 14:44:47 +0100

    npm outdated
    ncu -u

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const config = require('config');
 // Load the SDK
 const RainbowSDK = require('rainbow-node-sdk');
 
-const Bot = require( './modules/bot')
+const Bot = require( './modules/bot');
 
 logger.info('Starting rainbow-lex-sample bot...');
 
@@ -23,16 +23,16 @@ bot = new Bot(lexOptions);
 
 // If the bot sends the sendIM event send it back to rainbow.
 bot.emitter.on( 'sendIM',  param => {
-    rainbowSDK.im.sendMessageToJid( param.message, param.user );    
+    rainbowSDK.im.sendMessageToJid( param.message, param.user );
 })
- 
+
 // When an IM message is received, give it to the bot for him to handle it.
-rainbowSDK.events.on('rainbow_onmessagereceived', (message) => {   
+rainbowSDK.events.on('rainbow_onmessagereceived', (message) => {
     if(message.type === "chat") {
         logger.debug( "before bot handleMessage")
         // Send to the bot
         bot.handleMessage({
-            message: message.content, 
+            message: message.content,
             user: message.fromJid
         });
         logger.debug( "after bot handleMessage")
